a759600371654f69aed4f6f51bc916f5de5a7716
indumadu <indumadu@gmail.com>
2019-02-09 20:20:17 -0500

    file converter and retriever

--- services/fileRetriever.py
+++ services/fileRetriever.py
@@ -3,3 +3,4 @@ def retrieveFromFtpServer( planet_name, image_file_path, meta_file_path, date_yy
     # store file in a location
 
 def retrieveFromHttpServer(planet_name, image_file_with_location, meta_file_path_name, data_yyyyMMdd, time_hhmm):
+ ##test

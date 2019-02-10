544a658088a67b4e62bfd3be361a608dbdef87d1
indumadu <indumadu@gmail.com>
2019-02-09 20:06:15 -0500

    file converter and retriever

---
+++ services/fileConverter.py
@@ -0,0 +1,4 @@
+def convertAnyImageToTiff():
+    return ''
+
+def
---
+++ services/fileRetriever.py
@@ -0,0 +1,5 @@
+
+def retrieveFromFtpServer( planet_name, image_file_path, meta_file_path, date_yyyyMMdd, time_hhmm):
+    # store file in a location
+
+def retrieveFromHttpServer(planet_name, image_file_with_location, meta_file_path_name, data_yyyyMMdd, time_hhmm):

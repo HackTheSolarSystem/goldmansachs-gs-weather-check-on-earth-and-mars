e168d54382130ebb1f887b475a191127d1d12458
Archana Muralimohan <archana.muralimohan@gmail.com>
2019-02-09 23:41:51 -0500

    correct path and updated formatting

--- services/file_retriever.py
+++ services/file_retriever.py
@@ -14,8 +14,8 @@ def retrieve_from_server(planet_name, image_file_path, meta_file_path, date_yyyy
     image_file_extension = get_extension(image_file_path)
     meta_file_extension = get_extension(meta_file_path)
 
-    urllib.request.urlretrieve(image_file_path, directory+image_file_name+image_file_extension)
-    urllib.request.urlretrieve(meta_file_path, directory+meta_file_name+meta_file_extension)
+    urllib.request.urlretrieve(image_file_path, directory + image_file_name + image_file_extension)
+    urllib.request.urlretrieve(meta_file_path, directory + meta_file_name + meta_file_extension)
 
     if image_file_extension != ".tiff":
         convert_to_tiff(directory, image_file_name, image_file_extension)
@@ -23,7 +23,7 @@ def retrieve_from_server(planet_name, image_file_path, meta_file_path, date_yyyy
 
 def get_directory(planet_name, date_yyyyMMdd):
     slash = "/"
-    directory = planet_name + slash + date_yyyyMMdd + slash
+    directory = 'data' + slash + planet_name + slash + date_yyyyMMdd + slash
     if not os.path.exists(directory):
         os.makedirs(directory)
     return directory
@@ -32,16 +32,16 @@ def get_filename(file_name, date_yyyyMMdd, time_hhmm):
     if file_name.find('/'):
         file_name_with_ext = file_name.rsplit('/', 1)[1]
         file_name = file_name_with_ext[0 : file_name_with_ext.index('.')]
-        return file_name+"_"+date_yyyyMMdd+"_"+time_hhmm
+        return file_name + "_" + date_yyyyMMdd + "_" + time_hhmm
 
 def get_extension(file_path):
     if file_path.find('.'):
-        return "."+file_path.rsplit('.', 1)[1]
+        return "." + file_path.rsplit('.', 1)[1]
 
 
 def convert_to_tiff(directory, image_file_name, image_file_extension):
-    img=Image.open(directory+image_file_name+image_file_extension)
-    img.save(directory+image_file_name+".tiff")
+    img=Image.open(directory + image_file_name + image_file_extension)
+    img.save(directory + image_file_name + ".tiff")
 
 if __name__ =='__main__':
     retrieve_from_server('mars', 'https://minnlawyer.com/files/2017/04/comma-clipart-clipart-best-frrJZJ-clipart.jpg', 'https://cdn.pixabay.com/photo/2017/01/03/02/07/vine-1948358_1280.png', '20180202', '0000')

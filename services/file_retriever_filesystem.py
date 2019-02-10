722c2768a1c72e1c017df8d5c214dfc22de4d58b
Emy Rivera <emymyrelis@gmail.com>
2019-02-10 12:32:23 -0500

    committing fix for file retriever

--- services/file_retriever_filesystem.py
+++ services/file_retriever_filesystem.py
@@ -4,49 +4,48 @@ from PIL import Image
 from shutil import copyfile
 
 def retrieve_from_server(planet_name, image_file_path, date_yyyyMMdd, time_hhmm):
-    #add null checks for all input params
-    planet_name = planet_name.upper()
-    directory = get_directory(planet_name, date_yyyyMMdd)
-    image_file_name=get_filename(image_file_path, date_yyyyMMdd, time_hhmm)
-    image_file_extension = get_extension(image_file_path)
+   #add null checks for all input params
+   planet_name = planet_name.upper()
+   directory = get_directory(planet_name, date_yyyyMMdd)
+   image_file_name=get_filename(image_file_path) + "_" + date_yyyyMMdd + "_" + time_hhmm
+   image_file_extension = get_extension(image_file_path)
 
-    print(image_file_path)
+   destination_file_name = directory + image_file_name + image_file_extension
+   copyfile(image_file_path, destination_file_name)
 
-    copyfile(image_file_path, directory)
-
-    if image_file_extension != ".tiff":
-        return convert_to_tiff(directory, image_file_name, image_file_extension)
-    else: 
-        return(image_file_name + image_file_extension)
+   if image_file_extension != ".tiff":
+       return convert_to_tiff(directory, image_file_name, image_file_extension)
+   else:
+       return(image_file_name + image_file_extension)
 
 
 def get_directory(planet_name, date_yyyyMMdd):
-    slash = "/"
-    directory = 'data' + slash + planet_name + slash + date_yyyyMMdd + slash
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    return directory
+   slash = "/"
+   directory = 'data' + slash + planet_name + slash + date_yyyyMMdd + slash
+   if not os.path.exists(directory):
+       os.makedirs(directory)
+   return directory
 
-def get_filename(file_name, date_yyyyMMdd, time_hhmm):
-    if file_name.find('/') != -1:
-        file_name_with_ext = file_name.rsplit('/', 1)[1]
-    else:
-        file_name_with_ext=file_name
-    file_name = file_name_with_ext[0 : file_name_with_ext.index('.')]
-    return file_name + "_" + date_yyyyMMdd + "_" + time_hhmm
+def get_filename(file_name):
+   if file_name.find('/') != -1:
+       file_name_with_ext = file_name.rsplit('/', 1)[1]
+   else:
+       file_name_with_ext=file_name
+   file_name = file_name_with_ext[0 : file_name_with_ext.index('.')]
+   return file_name
 
 def get_extension(file_path):
-    if file_path.find('.') != -1:
-        return "." + file_path.rsplit('.', 1)[1]
-    else:
-        return "."
+   if file_path.find('.') != -1:
+       return "." + file_path.rsplit('.', 1)[1]
+   else:
+       return "."
 
 
 def convert_to_tiff(directory, image_file_name, image_file_extension):
-    img=Image.open(directory + image_file_name + image_file_extension)
-    tiff_file_name = image_file_name+".tiff"
-    img.save(directory + tiff_file_name)
-    return tiff_file_name
+   img=Image.open(directory + image_file_name + image_file_extension)
+   tiff_file_name = image_file_name+".tiff"
+   img.save(directory + tiff_file_name)
+   return tiff_file_name
 
 if __name__ =='__main__':
-    retrieve_from_server('mars', 'vine-1948358_1280_20180202_0000.png', '20180202', '0000')
+   retrieve_from_server('earth', 'vine-1948358_1280_20180202_0000.png', '20180202', '0000')
